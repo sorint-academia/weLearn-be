5d9abc0b1859a006b053208ec1e9cab44a3ebee0
amreo <amreo@live.it>
2018-04-06 11:29:15 +0200

    Added template for isStudentOf and isTeacherOf

--- src/main/java/it/sorint/welearnbe/services/SessionService.java
+++ src/main/java/it/sorint/welearnbe/services/SessionService.java
@@ -1,5 +1,18 @@
 package it.sorint.welearnbe.services;
 
-public class SessionService {
+import java.util.UUID;
 
+public class SessionService {
+	
+	//_course_student_ 
+	public boolean isStudentOf(String user, UUID courseID) {
+		//TODO: implement this function
+		return true;
+	}
+	//_course_student_ 
+	public boolean isTeacherOf(String user, UUID courseID) {
+		//TODO: implement this function
+		return true;
+	}
+	
 }
