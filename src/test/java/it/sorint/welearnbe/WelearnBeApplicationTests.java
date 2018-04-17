1236e3b3d158278548b33f21a15243038ad6a0d2
Walter Bosis <wbosis@sorint.it>
2018-04-17 10:43:46 +0200

    delete comment

--- src/test/java/it/sorint/welearnbe/WelearnBeApplicationTests.java
+++ src/test/java/it/sorint/welearnbe/WelearnBeApplicationTests.java
@@ -16,7 +16,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @DataMongoTest
-//@Import(TestMongoConfig.class)
 public class WelearnBeApplicationTests {
 
 	@Autowired
