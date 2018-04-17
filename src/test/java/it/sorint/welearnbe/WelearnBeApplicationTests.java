012682adc5d4a8f5d16f4b1e6484e7e6d96a782a
Walter Bosis <wbosis@sorint.it>
2018-04-17 09:34:51 +0200

    add @DataMongoTest to WelearnBeApplicationTest. Add application.properties under test/resources

--- src/test/java/it/sorint/welearnbe/WelearnBeApplicationTests.java
+++ src/test/java/it/sorint/welearnbe/WelearnBeApplicationTests.java
@@ -1,14 +1,40 @@
 package it.sorint.welearnbe;
 
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@DataMongoTest
+//@Import(TestMongoConfig.class)
 public class WelearnBeApplicationTests {
 
+	@Autowired
+    private MongoTemplate mongoTemplate;
+	
+	
+	
+    @Before
+    public void setUp() throws Exception {
+    	mongoTemplate.dropCollection(WelearnBeApplication.class);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    	mongoTemplate.dropCollection(WelearnBeApplication.class);
+    }
+
+	
 	@Test
 	public void contextLoads() {
 	}
