2bb19f789a605b5e96e22028b3f84cbab883b313
amreo <amreo@live.it>
2018-05-21 08:21:03 +0200

    Fixed method signature

--- src/main/java/it/sorint/welearnbe/controllers/BuildAndExecController.java
+++ src/main/java/it/sorint/welearnbe/controllers/BuildAndExecController.java
@@ -5,6 +5,8 @@ import java.io.InputStream;
 import java.security.Principal;
 import java.util.UUID;
 
+import javax.xml.ws.Response;
+
 import org.apache.commons.io.IOUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,28 +23,27 @@ import it.sorint.welearnbe.controllers.entity.BuildResultFE;
 public class BuildAndExecController {
 
 	@PostMapping("/progresses/{progressID}/projects/{projectID}/build")
-	public BuildResultFE buildProgressProject(Principal principal, @PathVariable("progressID") UUID progressID, @PathVariable("projectID") UUID projectID) {
+	public ResponseEntity<BuildResultFE> buildProgressProject(Principal principal, @PathVariable("progressID") UUID progressID, @PathVariable("projectID") UUID projectID) {
 		return null;		
 	}
 	
-	@SuppressWarnings("rawtypes")
 	@PostMapping("/progresses/{progressID}/projects/{projectID}/exec")
-	public ResponseEntity execProgressProject(Principal principal, @PathVariable("progressID") UUID progressID, @PathVariable("projectID") UUID projectID) {
+	public ResponseEntity<Void> execProgressProject(Principal principal, @PathVariable("progressID") UUID progressID, @PathVariable("projectID") UUID projectID) {
 		return null;		
 	}
 
 	@GetMapping("/progresses/{progressID}/projects/{projectID}/stdout")
-	public Byte[] stdoutProgressProject(Principal principal, @PathVariable("progressID") UUID progressID, @PathVariable("projectID") UUID projectID) {
+	public ResponseEntity<Byte[]> stdoutProgressProject(Principal principal, @PathVariable("progressID") UUID progressID, @PathVariable("projectID") UUID projectID) {
 		return null;		
 	}
 
 	@GetMapping("/progresses/{progressID}/projects/{projectID}/stderr")
-	public Byte[] stderrProgressProject(Principal principal, @PathVariable("progressID") UUID progressID, @PathVariable("projectID") UUID projectID) {
+	public ResponseEntity<Byte[]> stderrProgressProject(Principal principal, @PathVariable("progressID") UUID progressID, @PathVariable("projectID") UUID projectID) {
 		return null;		
 	}
 
 	@PutMapping("/progresses/{progressID}/projects/{projectID}/stdin")
-	public Byte[] writeStdinProgressProject(Principal principal, @PathVariable("progressID") UUID progressID, @PathVariable("projectID") UUID projectID, InputStream stream) throws IOException {
+	public ResponseEntity<Byte[]> writeStdinProgressProject(Principal principal, @PathVariable("progressID") UUID progressID, @PathVariable("projectID") UUID projectID, InputStream stream) throws IOException {
 		//Get stream content
 		byte[] content = IOUtils.toByteArray(stream);
 		
