476be907a3fe623ec11a29cf9b2798954576e39b
amreo <amreo@live.it>
2018-05-24 21:14:25 +0200

    Implemented GET /projects

--- src/main/java/it/sorint/welearnbe/controllers/ProjectController.java
+++ src/main/java/it/sorint/welearnbe/controllers/ProjectController.java
@@ -1,10 +1,29 @@
 package it.sorint.welearnbe.controllers;
 
+import java.security.Principal;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import it.sorint.welearnbe.controllers.entity.ProjectFE;
+import it.sorint.welearnbe.converter.ProjectConverter;
+import it.sorint.welearnbe.services.ProjectService;
 
 @RestController
 @RequestMapping("/api")
 public class ProjectController {
 	
+	@Autowired
+	ProjectService projectService;
+	
+	@GetMapping("/projects")
+	public List<ProjectFE> getProjects(Principal principal) {
+		return projectService.getProjects(principal.getName()).stream()
+				.map(be -> ProjectConverter.convertToProjectFE(be))
+				.collect(Collectors.toList());
+	}
 }
--- src/main/java/it/sorint/welearnbe/services/ProjectService.java
+++ src/main/java/it/sorint/welearnbe/services/ProjectService.java
@@ -1,8 +1,21 @@
 package it.sorint.welearnbe.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import it.sorint.welearnbe.repository.ProjectRepository;
+import it.sorint.welearnbe.repository.entity.ProjectBE;
 
 @Service
 public class ProjectService {
-
+	
+	@Autowired
+	ProjectRepository projectRepository;
+	
+	public List<ProjectBE> getProjects(String username) {
+		//FIXME: limit visibility to only viewable projects
+		return projectRepository.findAll();
+	}
 }
